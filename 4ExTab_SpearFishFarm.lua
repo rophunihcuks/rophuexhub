83a6bf3b17a85ceb5dd09a2c89da93e72db5e1f2
rophunihcuks <ksanihtest@gmail.com>
2026-01-14 01:29:30 +0800

    Update 4ExTab_SpearFishFarm.lua

--- 4ExTab_SpearFishFarm.lua
+++ 4ExTab_SpearFishFarm.lua
@@ -1869,7 +1869,7 @@ local function createMainLayout()
     title.TextColor3 = Color3.fromRGB(255, 255, 255)
     title.Position = UDim2.new(0, 14, 0, 4)
     title.Size = UDim2.new(1, -28, 0, 20)
-    title.Text = "Spear Fish Farm V1. (LITE 3)"
+    title.Text = "Spear Fish Farm V1.3 (LITE)"
 
     local subtitle = Instance.new("TextLabel")
     subtitle.Name = "Subtitle"
